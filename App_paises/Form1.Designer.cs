074c29bb648b8a189b8295bff2d773d96267ee86
genesis castellano <alexaguerra184@gmail.com>
2024-05-23 14:54:00 -0700

    Add files via upload

---
+++ App_paises/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace App_paises
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_mostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Estados Unidos",
+            "España",
+            "Republica Dominicana",
+            "Colombia"});
+            this.comboBox1.Location = new System.Drawing.Point(280, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(280, 144);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 224);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_mostrar
+            // 
+            this.btn_mostrar.Location = new System.Drawing.Point(620, 144);
+            this.btn_mostrar.Name = "btn_mostrar";
+            this.btn_mostrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_mostrar.TabIndex = 2;
+            this.btn_mostrar.Text = "Mostrar";
+            this.btn_mostrar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_mostrar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_mostrar;
+    }
+}
+
---
+++ App_paises/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_paises
+{
+    public partial class Form1 : Form
+    {
+        // Diccionario para almacenar la información de los países
+        private Dictionary<string, string> countryInfo = new Dictionary<string, string>()
+            {
+            { "Seleccionar país", "" },
+            { "Estados Unidos", "Capital: Washington, D.C.\r\nPoblación: 331 millones\r\nIdioma: Inglés" },
+            { "España", "Capital: Madrid\r\nPoblación: 47 millones\r\nIdioma: Español" },
+            { "argentina", "Capital: Ciudad de Buenos Aires\r\nPoblación: 46 millones\r\nIdioma: Español" },
+            { "Republica Dominicana", "Capital: Santo Domingo\r\nPoblación: 11.12 millones\r\nIdioma: Español" },
+            { "Colombia", "Capital: Bogota\r\nPoblación: 52 millones\r\nIdioma: Español" }
+            // Agrega más países según sea necesario
+        };
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeComponent();
+            // Agrega los países al ComboBox
+            foreach (var country in countryInfo.Keys)
+            {
+                comboBox1.Items.Add(country);
+            }
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
