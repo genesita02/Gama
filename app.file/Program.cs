cd3f2f189b0dff5aca8163dbd26d95539375a9bf
genesis castellano <alexaguerra184@gmail.com>
2024-05-23 14:44:01 -0700

    Add files via upload

---
+++ app.file/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("GESTION DE ARCHIVOS");
+
+Console.WriteLine();
+
+//Creacion de archivo
+
+StreamWriter archivo = new StreamWriter("archivo.txt");
+archivo.WriteLine("Contenido del archivo.");
+archivo.WriteLine("Contenido en otra linea.");
+archivo.Close();
