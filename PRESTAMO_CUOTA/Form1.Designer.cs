d814ae4345d9cd756173a3ce1a92cf312ced9d59
genesis castellano <alexaguerra184@gmail.com>
2024-05-23 19:32:15 -0700

    Add files via upload

---
+++ PRESTAMO_CUOTA/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace PRESTAMO_CUOTA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_monto_prestamo = new System.Windows.Forms.TextBox();
+            this.txt_plazo_meses = new System.Windows.Forms.TextBox();
+            this.txt_tasa_interes = new System.Windows.Forms.TextBox();
+            this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.dgv_prestamo_cuota = new System.Windows.Forms.DataGridView();
+            this.cuota = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.capital = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.interes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.saldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dlg_guarda = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_prestamo_cuota)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MONTO_PRESTAMO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PLAZO_MESES";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TASA_INTERES";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "RESULTADO";
+            // 
+            // txt_monto_prestamo
+            // 
+            this.txt_monto_prestamo.Location = new System.Drawing.Point(207, 12);
+            this.txt_monto_prestamo.Name = "txt_monto_prestamo";
+            this.txt_monto_prestamo.Size = new System.Drawing.Size(100, 20);
+            this.txt_monto_prestamo.TabIndex = 4;
+            // 
+            // txt_plazo_meses
+            // 
+            this.txt_plazo_meses.Location = new System.Drawing.Point(207, 65);
+            this.txt_plazo_meses.Name = "txt_plazo_meses";
+            this.txt_plazo_meses.Size = new System.Drawing.Size(100, 20);
+            this.txt_plazo_meses.TabIndex = 5;
+            // 
+            // txt_tasa_interes
+            // 
+            this.txt_tasa_interes.Location = new System.Drawing.Point(207, 130);
+            this.txt_tasa_interes.Name = "txt_tasa_interes";
+            this.txt_tasa_interes.Size = new System.Drawing.Size(100, 20);
+            this.txt_tasa_interes.TabIndex = 6;
+            // 
+            // txt_resultado
+            // 
+            this.txt_resultado.Location = new System.Drawing.Point(207, 191);
+            this.txt_resultado.Name = "txt_resultado";
+            this.txt_resultado.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado.TabIndex = 7;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.Location = new System.Drawing.Point(641, 12);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(112, 23);
+            this.btn_calcular.TabIndex = 8;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(641, 65);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(112, 23);
+            this.btn_limpiar.TabIndex = 9;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_guardar.Location = new System.Drawing.Point(641, 122);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(112, 23);
+            this.btn_guardar.TabIndex = 10;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(641, 178);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(112, 23);
+            this.btn_salir.TabIndex = 11;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // dgv_prestamo_cuota
+            // 
+            this.dgv_prestamo_cuota.AllowUserToAddRows = false;
+            this.dgv_prestamo_cuota.AllowUserToDeleteRows = false;
+            this.dgv_prestamo_cuota.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_prestamo_cuota.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cuota,
+            this.capital,
+            this.interes,
+            this.saldo});
+            this.dgv_prestamo_cuota.Location = new System.Drawing.Point(18, 267);
+            this.dgv_prestamo_cuota.Name = "dgv_prestamo_cuota";
+            this.dgv_prestamo_cuota.ReadOnly = true;
+            this.dgv_prestamo_cuota.Size = new System.Drawing.Size(494, 150);
+            this.dgv_prestamo_cuota.TabIndex = 12;
+            // 
+            // cuota
+            // 
+            this.cuota.HeaderText = "Cuota";
+            this.cuota.Name = "cuota";
+            this.cuota.ReadOnly = true;
+            // 
+            // capital
+            // 
+            this.capital.HeaderText = "Capital";
+            this.capital.Name = "capital";
+            this.capital.ReadOnly = true;
+            // 
+            // interes
+            // 
+            this.interes.HeaderText = "Interes";
+            this.interes.Name = "interes";
+            this.interes.ReadOnly = true;
+            // 
+            // saldo
+            // 
+            this.saldo.HeaderText = "Saldo";
+            this.saldo.Name = "saldo";
+            this.saldo.ReadOnly = true;
+            this.saldo.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv_prestamo_cuota);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_resultado);
+            this.Controls.Add(this.txt_tasa_interes);
+            this.Controls.Add(this.txt_plazo_meses);
+            this.Controls.Add(this.txt_monto_prestamo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_prestamo_cuota)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_monto_prestamo;
+        private System.Windows.Forms.TextBox txt_plazo_meses;
+        private System.Windows.Forms.TextBox txt_tasa_interes;
+        private System.Windows.Forms.TextBox txt_resultado;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.DataGridView dgv_prestamo_cuota;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cuota;
+        private System.Windows.Forms.DataGridViewTextBoxColumn capital;
+        private System.Windows.Forms.DataGridViewTextBoxColumn interes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn saldo;
+        private System.Windows.Forms.SaveFileDialog dlg_guarda;
+    }
+}
+
---
+++ PRESTAMO_CUOTA/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRESTAMO_CUOTA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            Double Monto, Tasa_Interes, Cuotas, Total;
+            int Años;
+
+            Monto = Double.Parse(txt_monto_prestamo.Text);
+            Años = int.Parse(txt_plazo_meses.Text);
+
+            Tasa_Interes = ((Monto * 16)/100) * Años;
+
+            Total = Monto + Tasa_Interes;
+            Cuotas = Total/(Años * 12);
+
+
+            txt_tasa_interes.Text = Tasa_Interes.ToString();
+            txt_resultado.Text = Total.ToString();
+
+            dgv_prestamo_cuota.Rows.Add(txt_monto_prestamo.Text, txt_plazo_meses.Text, txt_resultado.Text, txt_tasa_interes.Text);
+
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_monto_prestamo.Clear();
+            txt_plazo_meses.Clear();
+            txt_tasa_interes.Clear();
+            txt_resultado.Clear();
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            if (dlg_guarda.ShowDialog() == DialogResult.OK) ;
+            {
+                string rutaArchivo = dlg_guarda.FileName;
+                string createxto = txt_monto_prestamo.Text;
+
+                StreamWriter archivo = File.CreateText(rutaArchivo);
+
+                archivo.Write(createxto);
+                archivo.Flush();
+                archivo.Close();
+            }
+
+
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
