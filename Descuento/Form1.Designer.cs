ccb04f034b0d09fa8c88e5680f4535063903e78c
genesis castellano <alexaguerra184@gmail.com>
2024-05-23 18:56:25 -0700

    Add files via upload

---
+++ Descuento/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Descuento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_descuento = new System.Windows.Forms.TextBox();
+            this.txt_itbis = new System.Windows.Forms.TextBox();
+            this.txt_monto_neto = new System.Windows.Forms.TextBox();
+            this.txt_monto_total = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.Btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DESCUENTO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ITBIS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MONTO NETO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MONTO TOTAL";
+            // 
+            // txt_descuento
+            // 
+            this.txt_descuento.Location = new System.Drawing.Point(191, 41);
+            this.txt_descuento.Name = "txt_descuento";
+            this.txt_descuento.Size = new System.Drawing.Size(148, 20);
+            this.txt_descuento.TabIndex = 4;
+            // 
+            // txt_itbis
+            // 
+            this.txt_itbis.Location = new System.Drawing.Point(191, 89);
+            this.txt_itbis.Name = "txt_itbis";
+            this.txt_itbis.Size = new System.Drawing.Size(148, 20);
+            this.txt_itbis.TabIndex = 5;
+            // 
+            // txt_monto_neto
+            // 
+            this.txt_monto_neto.Location = new System.Drawing.Point(191, 148);
+            this.txt_monto_neto.Name = "txt_monto_neto";
+            this.txt_monto_neto.Size = new System.Drawing.Size(148, 20);
+            this.txt_monto_neto.TabIndex = 6;
+            // 
+            // txt_monto_total
+            // 
+            this.txt_monto_total.Location = new System.Drawing.Point(191, 213);
+            this.txt_monto_total.Name = "txt_monto_total";
+            this.txt_monto_total.Size = new System.Drawing.Size(148, 20);
+            this.txt_monto_total.TabIndex = 7;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.Location = new System.Drawing.Point(461, 40);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(130, 26);
+            this.btn_calcular.TabIndex = 8;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(461, 111);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(130, 26);
+            this.btn_limpiar.TabIndex = 9;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // Btn_salir
+            // 
+            this.Btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_salir.Location = new System.Drawing.Point(461, 185);
+            this.Btn_salir.Name = "Btn_salir";
+            this.Btn_salir.Size = new System.Drawing.Size(130, 26);
+            this.Btn_salir.TabIndex = 10;
+            this.Btn_salir.Text = "Salir";
+            this.Btn_salir.UseVisualStyleBackColor = true;
+            this.Btn_salir.Click += new System.EventHandler(this.Btn_salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 263);
+            this.Controls.Add(this.Btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_monto_total);
+            this.Controls.Add(this.txt_monto_neto);
+            this.Controls.Add(this.txt_itbis);
+            this.Controls.Add(this.txt_descuento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_descuento;
+        private System.Windows.Forms.TextBox txt_itbis;
+        private System.Windows.Forms.TextBox txt_monto_neto;
+        private System.Windows.Forms.TextBox txt_monto_total;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button Btn_salir;
+    }
+}
+
---
+++ Descuento/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Descuento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            Decimal montoTotal;
+
+            if (decimal.TryParse(txt_monto_total.Text, out montoTotal))
+            {
+                decimal descuento = CalcularDescuento(montoTotal);
+                decimal itbis = CalcularITBIS(montoTotal);
+                decimal montoNeto = montoTotal - descuento + itbis;
+
+                if (descuento > 0)
+                {
+                    MessageBox.Show($"Se aplicará un descuento de {descuento:C}\n" +
+                                    $"El ITBIS a aplicar es: {itbis:C}\n" +
+                                    $"El monto neto a pagar es: {montoNeto:C}",
+                                    "Información de Pago");
+                }
+                else
+                {
+                    MessageBox.Show("No aplica descuento.", "Información de Pago");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un monto válido.", "Error de Entrada");
+            }
+        }
+
+        private decimal CalcularDescuento(decimal montoTotal)
+        {
+            decimal descuento = 0;
+
+            if (montoTotal >= 1001 && montoTotal <= 10000)
+            {
+                descuento = montoTotal * 0.03m;
+            }
+            else if (montoTotal > 10000 && montoTotal <= 15000)
+            {
+                descuento = montoTotal * 0.05m;
+            }
+            else if (montoTotal > 15000 && montoTotal <= 20000)
+            {
+                descuento = montoTotal * 0.08m;
+            }
+            else if (montoTotal > 20000)
+            {
+                descuento = montoTotal * 0.10m;
+            }
+
+            return descuento;
+        }
+
+        private decimal CalcularITBIS(decimal montoTotal)
+        {
+            // Supondremos que el ITBIS es el 18% del monto total
+            return montoTotal * 0.18m;
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_descuento.Clear();
+            txt_itbis.Clear();
+            txt_monto_neto.Clear();
+            txt_monto_total.Clear();
+        }
+
+        private void Btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
