0da329aa65b68c8bf7caef04c73f607ae76b8040
genesis castellano <alexaguerra184@gmail.com>
2024-05-23 17:11:12 -0700

    Add files via upload

---
+++ numero par o impar/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("par o impar");
+
+    {
+        // Solicitar al usuario que ingrese un número
+        Console.Write("Ingrese un número entero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        // Determinar si el número es par o impar
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"{numero} es un número par.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} es un número impar.");
+        }
+    }
+
+
