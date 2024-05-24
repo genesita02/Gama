731bf475f19628e5a5f4e9c94539cbb007f9cb31
genesis castellano <alexaguerra184@gmail.com>
2024-05-23 18:26:18 -0700

    Add files via upload

---
+++ COMPLETIVO/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace COMPLETIVO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txt_nota_1 = new System.Windows.Forms.TextBox();
+            this.txt_nota_2 = new System.Windows.Forms.TextBox();
+            this.txt_nota_3 = new System.Windows.Forms.TextBox();
+            this.txt_nota_4 = new System.Windows.Forms.TextBox();
+            this.txt_promedio = new System.Windows.Forms.TextBox();
+            this.txt_situacion = new System.Windows.Forms.TextBox();
+            this.txt_nota_completiva = new System.Windows.Forms.TextBox();
+            this.txt_prueba_completiva = new System.Windows.Forms.TextBox();
+            this.txt_promedio_completivo = new System.Windows.Forms.TextBox();
+            this.txt_situacion_completiva = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOTA 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOTA 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NOTA 3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NOTA 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "PROMEDIO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 246);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "SITUACION";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 295);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "NOTA_COMPLETIVA";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 386);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "PRUEBA _COMPLETIVA";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(286, 382);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(140, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "PROMEDIO_COMPLETIVO";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 428);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(139, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "SITUACION_COMPLETIVA";
+            // 
+            // txt_nota_1
+            // 
+            this.txt_nota_1.Location = new System.Drawing.Point(133, 9);
+            this.txt_nota_1.Name = "txt_nota_1";
+            this.txt_nota_1.Size = new System.Drawing.Size(100, 20);
+            this.txt_nota_1.TabIndex = 10;
+            // 
+            // txt_nota_2
+            // 
+            this.txt_nota_2.Location = new System.Drawing.Point(133, 51);
+            this.txt_nota_2.Name = "txt_nota_2";
+            this.txt_nota_2.Size = new System.Drawing.Size(100, 20);
+            this.txt_nota_2.TabIndex = 11;
+            // 
+            // txt_nota_3
+            // 
+            this.txt_nota_3.Location = new System.Drawing.Point(133, 96);
+            this.txt_nota_3.Name = "txt_nota_3";
+            this.txt_nota_3.Size = new System.Drawing.Size(100, 20);
+            this.txt_nota_3.TabIndex = 12;
+            // 
+            // txt_nota_4
+            // 
+            this.txt_nota_4.Location = new System.Drawing.Point(133, 143);
+            this.txt_nota_4.Name = "txt_nota_4";
+            this.txt_nota_4.Size = new System.Drawing.Size(100, 20);
+            this.txt_nota_4.TabIndex = 13;
+            // 
+            // txt_promedio
+            // 
+            this.txt_promedio.Location = new System.Drawing.Point(133, 197);
+            this.txt_promedio.Name = "txt_promedio";
+            this.txt_promedio.Size = new System.Drawing.Size(100, 20);
+            this.txt_promedio.TabIndex = 14;
+            // 
+            // txt_situacion
+            // 
+            this.txt_situacion.Location = new System.Drawing.Point(133, 246);
+            this.txt_situacion.Name = "txt_situacion";
+            this.txt_situacion.Size = new System.Drawing.Size(100, 20);
+            this.txt_situacion.TabIndex = 15;
+            // 
+            // txt_nota_completiva
+            // 
+            this.txt_nota_completiva.Location = new System.Drawing.Point(133, 292);
+            this.txt_nota_completiva.Name = "txt_nota_completiva";
+            this.txt_nota_completiva.Size = new System.Drawing.Size(100, 20);
+            this.txt_nota_completiva.TabIndex = 16;
+            // 
+            // txt_prueba_completiva
+            // 
+            this.txt_prueba_completiva.Location = new System.Drawing.Point(153, 379);
+            this.txt_prueba_completiva.Name = "txt_prueba_completiva";
+            this.txt_prueba_completiva.Size = new System.Drawing.Size(100, 20);
+            this.txt_prueba_completiva.TabIndex = 17;
+            // 
+            // txt_promedio_completivo
+            // 
+            this.txt_promedio_completivo.Location = new System.Drawing.Point(478, 375);
+            this.txt_promedio_completivo.Name = "txt_promedio_completivo";
+            this.txt_promedio_completivo.Size = new System.Drawing.Size(100, 20);
+            this.txt_promedio_completivo.TabIndex = 18;
+            // 
+            // txt_situacion_completiva
+            // 
+            this.txt_situacion_completiva.Location = new System.Drawing.Point(279, 428);
+            this.txt_situacion_completiva.Name = "txt_situacion_completiva";
+            this.txt_situacion_completiva.Size = new System.Drawing.Size(100, 20);
+            this.txt_situacion_completiva.TabIndex = 19;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(575, 40);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_calcular.TabIndex = 20;
+            this.btn_calcular.Text = "Calcular Completivo";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(575, 85);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpiar.TabIndex = 21;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(575, 143);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(75, 23);
+            this.btn_salir.TabIndex = 22;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_situacion_completiva);
+            this.Controls.Add(this.txt_promedio_completivo);
+            this.Controls.Add(this.txt_prueba_completiva);
+            this.Controls.Add(this.txt_nota_completiva);
+            this.Controls.Add(this.txt_situacion);
+            this.Controls.Add(this.txt_promedio);
+            this.Controls.Add(this.txt_nota_4);
+            this.Controls.Add(this.txt_nota_3);
+            this.Controls.Add(this.txt_nota_2);
+            this.Controls.Add(this.txt_nota_1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txt_nota_1;
+        private System.Windows.Forms.TextBox txt_nota_2;
+        private System.Windows.Forms.TextBox txt_nota_3;
+        private System.Windows.Forms.TextBox txt_nota_4;
+        private System.Windows.Forms.TextBox txt_promedio;
+        private System.Windows.Forms.TextBox txt_situacion;
+        private System.Windows.Forms.TextBox txt_nota_completiva;
+        private System.Windows.Forms.TextBox txt_prueba_completiva;
+        private System.Windows.Forms.TextBox txt_promedio_completivo;
+        private System.Windows.Forms.TextBox txt_situacion_completiva;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
+
