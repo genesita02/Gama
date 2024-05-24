d045b93572d096c66f8dde7ce6d57412f9a8f678
genesis castellano <alexaguerra184@gmail.com>
2024-05-23 19:39:42 -0700

    Add files via upload

---
+++ Tabla_de_Multiplicacion/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Tabla_de_Multiplicacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.Btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.list_Multiplica = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_tabla = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.Location = new System.Drawing.Point(511, 144);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(114, 23);
+            this.btn_calcular.TabIndex = 0;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // Btn_limpiar
+            // 
+            this.Btn_limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_limpiar.Location = new System.Drawing.Point(511, 199);
+            this.Btn_limpiar.Name = "Btn_limpiar";
+            this.Btn_limpiar.Size = new System.Drawing.Size(114, 23);
+            this.Btn_limpiar.TabIndex = 1;
+            this.Btn_limpiar.Text = "Limpiar";
+            this.Btn_limpiar.UseVisualStyleBackColor = true;
+            this.Btn_limpiar.Click += new System.EventHandler(this.Btn_limpiar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(511, 254);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(114, 23);
+            this.btn_salir.TabIndex = 2;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // list_Multiplica
+            // 
+            this.list_Multiplica.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list_Multiplica.FormattingEnabled = true;
+            this.list_Multiplica.ItemHeight = 16;
+            this.list_Multiplica.Location = new System.Drawing.Point(263, 91);
+            this.list_Multiplica.Name = "list_Multiplica";
+            this.list_Multiplica.Size = new System.Drawing.Size(174, 228);
+            this.list_Multiplica.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(40, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tabla de Multiplicar";
+            // 
+            // txt_tabla
+            // 
+            this.txt_tabla.Location = new System.Drawing.Point(481, 103);
+            this.txt_tabla.Name = "txt_tabla";
+            this.txt_tabla.Size = new System.Drawing.Size(168, 20);
+            this.txt_tabla.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(735, 383);
+            this.Controls.Add(this.txt_tabla);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.list_Multiplica);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.Btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button Btn_limpiar;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.ListBox list_Multiplica;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_tabla;
+    }
+}
+
---
+++ Tabla_de_Multiplicacion/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabla_de_Multiplicacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            {
+                if (int.TryParse(txt_tabla.Text, out int numero))
+                {
+                    list_Multiplica.Items.Clear();
+                    for (int i = 1; i <= 12; i++)
+                    {
+                        int resultado = numero * i;
+                        list_Multiplica.Items.Add($"{numero} x {i} = {resultado}");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Por favor ingrese un número válido.");
+                }
+
+            }
+
+        }
+
+        private void Btn_limpiar_Click(object sender, EventArgs e)
+        {
+            list_Multiplica.Items.Clear();
+            txt_tabla.Clear();
+
+
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
